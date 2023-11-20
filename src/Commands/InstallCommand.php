494771155fa91fb0a19eff1c61f8ee75ede890b6
Inveteratus <alan.mcfarlane@gmail.com>
2023-11-20 13:20:20 +0000

    Coorected installer

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -43,35 +43,35 @@ class InstallCommand extends Command
             base_path('postcss.config.js')
         );
 
-//        $fs->copy(
-//            __DIR__ . '/../../stubs/tailwind.config.js',
-//            base_path('tailwind.config.js')
-//        );
-//
-//        $fs->copyDirectory(
-//            __DIR__ . '/../../stubs/app/Http/Controllers/Acme',
-//            base_path('app/Http/Controllers')
-//        );
-//
-//        $fs->copyDirectory(
-//            __DIR__ . '/../../stubs/resources/css/acme',
-//            base_path('resources/css')
-//        );
-//
-//        $fs->copyDirectory(
-//            __DIR__ . '/../../stubs/resources/js/acme',
-//            base_path('resources/js')
-//        );
-//
-//        $fs->copyDirectory(
-//            __DIR__ . '/../../stubs/resources/views/acme',
-//            base_path('resources/views')
-//        );
-//
-//        $fs->copy(
-//            __DIR__ . '/../../stubs/routes/acme.php',
-//            base_path('routes')
-//        );
+        $fs->copy(
+            __DIR__ . '/../../stubs/tailwind.config.js',
+            base_path('tailwind.config.js')
+        );
+
+        $fs->copyDirectory(
+            __DIR__ . '/../../stubs/app/Http/Controllers/Acme',
+            base_path('app/Http/Controllers')
+        );
+
+        $fs->copyDirectory(
+            __DIR__ . '/../../stubs/resources/css/acme',
+            base_path('resources/css')
+        );
+
+        $fs->copyDirectory(
+            __DIR__ . '/../../stubs/resources/js/acme',
+            base_path('resources/js')
+        );
+
+        $fs->copyDirectory(
+            __DIR__ . '/../../stubs/resources/views/acme',
+            base_path('resources/views')
+        );
+
+        $fs->copy(
+            __DIR__ . '/../../stubs/routes/acme.php',
+            base_path('routes/acme.php')
+        );
     }
 }
 
